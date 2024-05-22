e01c7843b6d4264fdd2b52d64b6b0c2bb669edb8
hauck97 <kevinhauck@outlook.de>
2024-05-22 23:37:36 +0200

    added sample routes

--- src/task-list/routes/web.php
+++ src/task-list/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return "hello world";
+});
+
+Route::get('/greeting/{name}', function ($name) {
+    return "hallo $name";
+});
