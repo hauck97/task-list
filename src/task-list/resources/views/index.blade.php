b4ad8ee98d0d423b51bd1cfde801bca1f5abd661
hauck97 <kevinhauck@outlook.de>
2024-05-24 20:54:47 +0200

    working on blade.template file

---
+++ src/task-list/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<h1>
+    Hello Im a blade template!
+</h1>
+
+<div>
+    @if(count($tasks))
+        <div>There are tasks!</div>
+    @else
+        <div>There are no tasks!</div>
+    @endif
+</div>
+
+
